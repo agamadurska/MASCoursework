c3dca36febc37435e671766ef322073bf2043554
Aga Madurska <amadurska@gmail.com>
2012-02-17 13:01:27 +0000

    Step 1: Implementing teh basics of a HospitalAgent

--- jadeCW/src/hospital/HospitalAgent.java
+++ jadeCW/src/hospital/HospitalAgent.java
@@ -1,6 +1,12 @@
 package hospital;
 
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPANames;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.Property;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 
 /**
  * Simple hospital agent, storing appointments and interacting
@@ -11,8 +17,43 @@ import jade.core.Agent;
  */
 public class HospitalAgent extends Agent{
 
+	/**
+	 * Register the maximum number of appointments.
+	 */
 	protected void setup() {
-	  	// Make this agent terminate
-	  	doDelete();
-	  } 
+		String serviceName = "allocate-appointments";
+	  	Object[] arguments =  getArguments();
+	  	int maxAppointments;
+
+	  	if (arguments != null) {
+	  		maxAppointments = (Integer) arguments[0];
+	  	} else {
+	  		// Terminate if created without arguments.
+	  		doDelete();
+	  		return;
+	  	}
+	  	
+	  	// Register the service
+	  	try {
+	  		DFAgentDescription agentDecription = new DFAgentDescription();
+	  		agentDecription.setName(getAID());
+	  		ServiceDescription serviceDescription = new ServiceDescription();
+	  		serviceDescription.setName(serviceName);
+
+	  		// Not sure if this is needed but was in example so keeping for now.
+	  		serviceDescription.setType("appointment-allocation");
+	  		serviceDescription.addOntologies("appointment-allocation-ontology");
+
+	  		// Agents that want to use this service need to "speak" the FIPA-SL language.
+	  		serviceDescription.addLanguages(FIPANames.ContentLanguage.FIPA_SL);
+	  		serviceDescription.addProperties(new Property("max-appointments", maxAppointments));
+
+	  		agentDecription.addServices(serviceDescription);
+	  		
+	  		DFService.register(this, agentDecription);
+	  	}
+	  	catch (FIPAException fe) {
+	  		fe.printStackTrace();
+	  	}
+	  }
 }
