f8866fa8d1e1d5eae5dad8f6adbab77c425f76e4
Aga Madurska <amadurska@gmail.com>
2012-02-17 16:34:17 +0000

    Step 2: Adding functionality for subscribing the patient agent to the allocate-appointments service. UNTESTED (not able to test till part 4 is complete)

--- jadeCW/src/hospital/PatientAgent.java
+++ jadeCW/src/hospital/PatientAgent.java
@@ -1,28 +1,75 @@
 package hospital;
 
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
 import utils.Appointment;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.proto.SubscriptionInitiator;
+import jade.util.leap.Iterator;
 
 public class PatientAgent extends Agent {
 	
 	private List<Appointment> priorities = new LinkedList<Appointment>();
 	private Appointment allocatedAppointment;
+	private AID provider;
 
 	protected void setup() {
 		Object[] arguments = getArguments();
 		if (arguments != null) {
 			buildPriorities((String) arguments[0]);
-			
+			subscribeToService("allocate-appointments");
 	  	} else {
 	  		// Terminate if created without arguments.
 	  		doDelete();
 	  		return;
 	  	}
+	}
+
+	private void subscribeToService(final String serviceType) {
+		// Build the description used as template for the subscription
+		DFAgentDescription template = new DFAgentDescription();
+		ServiceDescription templateSd = new ServiceDescription();
+		templateSd.setType(serviceType);
+		template.addServices(templateSd);
+  		
+		addBehaviour(new SubscriptionInitiator(this, DFService.createSubscriptionMessage(this, getDefaultDF(), template, null)) {
+			protected void handleInform(ACLMessage inform) {
+
+  			try {
+				DFAgentDescription[] results = DFService.decodeNotification(inform.getContent());
+		  		if (results.length > 0) {
+		  			for (int i = 0; i < results.length; ++i) {
+		  				DFAgentDescription dfd = results[i];
+		  				AID provider = dfd.getName();
+		  				// The same agent may provide several services;
+		  				Iterator it = dfd.getAllServices();
+		  				while (it.hasNext()) {
+		  					ServiceDescription sd = (ServiceDescription) it.next();
+		  					if (sd.getType().equals(serviceType)) {
+		  						storeProvider(provider);
+		  					}
+		  				}
+		  			}
+		  		}	
+	  			System.out.println();
+		  	}
+		  	catch (FIPAException fe) {
+		  		fe.printStackTrace();
+		  	}
+			}
+		} );
+	}
+
+	protected void storeProvider(AID provider) {
+		this.provider = provider;
 	}
 
 	private void buildPriorities(String preferences) {
